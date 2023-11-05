3fedd18dfb85d562e45c74bfc916c30ac7bd6cd4
Ananya Pal <palananya08@gmail.com>
2023-11-05 13:21:24 +0530

    js starts

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Welcome to Musical");
+let songs = [
+    {songName: "Salam-e-Ishq", filePath: "./Musical-Items/songs/1.mp3", coverPath: "./Musical-Items/covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "./Musical-Items/songs/1.mp3", coverPath: "./Musical-Items/covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "./Musical-Items/songs/1.mp3", coverPath: "./Musical-Items/covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "./Musical-Items/songs/1.mp3", coverPath: "./Musical-Items/covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "./Musical-Items/songs/1.mp3", coverPath: "./Musical-Items/covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "./Musical-Items/songs/1.mp3", coverPath: "./Musical-Items/covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "./Musical-Items/songs/1.mp3", coverPath: "./Musical-Items/covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "./Musical-Items/songs/1.mp3", coverPath: "./Musical-Items/covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "./Musical-Items/songs/1.mp3", coverPath: "./Musical-Items/covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "./Musical-Items/songs/1.mp3", coverPath: "./Musical-Items/covers/1.jpg"},
+]
+let audioElement = new Audio('./Musical-Items/songs/1.mp3');
+//audioElement.play()
